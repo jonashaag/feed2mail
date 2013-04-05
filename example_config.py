43cd48b85593b441490826a18ee47aca8ae2bcc0
Jonas Haag <jonas@lophus.org>
2013-04-06 02:41:43 +0300

    English

--- example_config.py
+++ example_config.py
@@ -4,7 +4,7 @@ SMTP_SERVER     = 'ser.ver.tld'
 SMTP_PORT       = None # can be absent/set to None for the default value
 
 # A list of feeds to fetch.
-# Items might be `(feed_url, group_name)` tuples.
+# Items may be `(feed_url, group_name)` tuples.
 # Entries of feeds that make a group won't be sent twice of they appear
 # on multiple feeds (often seen on News Sites that offer topic feeds)
 FEEDS = [
